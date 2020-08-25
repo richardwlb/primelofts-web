1f3c4570e1273b5893360119cf4752eb9b6c078c
Richard W. L. Brehmer <richardbrehmer@Richards-MacBook-Pro.local>
2020-08-24 21:25:57 -0300

    Heroku

---
+++ build/precache-manifest.8619eef9944636bbad0e9dfd2eb0fef0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d4b7653f9aa63c57f981be8fa84e6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "521fcd5320fbef84c991",
+    "url": "/static/css/2.543da19f.chunk.css"
+  },
+  {
+    "revision": "82349549dcb8471aaa52",
+    "url": "/static/css/main.f6026803.chunk.css"
+  },
+  {
+    "revision": "521fcd5320fbef84c991",
+    "url": "/static/js/2.ffca0d9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffca0d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82349549dcb8471aaa52",
+    "url": "/static/js/main.1d4b038b.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "c5d8c5e7f18588709a9940981db2e9ad",
+    "url": "/static/media/home-benef.c5d8c5e7.png"
+  },
+  {
+    "revision": "c64595613bdafa20dccc5e75271fc13b",
+    "url": "/static/media/img1.c6459561.jpg"
+  },
+  {
+    "revision": "58986750d09d4b7f7d5705b6b2ce21ac",
+    "url": "/static/media/img2.58986750.jpg"
+  },
+  {
+    "revision": "cb331afa877ee4c28cba8cc69c5e2b52",
+    "url": "/static/media/img3.cb331afa.jpg"
+  }
+]);
